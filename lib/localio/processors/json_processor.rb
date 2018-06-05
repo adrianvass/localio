f0645ab37277212d6aabda4206ea40d8bd6016da
Adrián Rubio <adrian.rubio@vass.es>
2018-06-05 14:12:45 +0200

    – Added the possibility of providing a channel through locfile – Added better terminal log output

--- lib/localio/processors/json_processor.rb
+++ lib/localio/processors/json_processor.rb
@@ -8,29 +8,38 @@ require 'json'
 class JsonProcessor
 
   def self.load_localizables(platform_options, options)
-
+    
     url = options[:url]
+    channel = options[:channel]
+
+    puts "-- LocalioVASS: Initializing. LocalioVASS will be querying #{url} for translations --"
 
     languages_array = options[:languages]
 
     default_language = 'es'
 
-    params = { channel: "all" }
+    params = { channel: channel.to_s }
 
     all_data = {}
     ordered_data = {}
 
     languages_array.each do |lang|
+      puts "-- LocalioVASS: Querying server for #{lang.to_s.capitalize!} language --"
+
       url_with_language_and_params = url + "#{lang}"
 
       url = URI.parse(url_with_language_and_params)
       url.query = URI.encode_www_form(params)
+
+      puts "-- LocalioVASS: Using URL: #{url}"
 
       response = Net::HTTP.get_response(url) 
       json = JSON.parse(response.body)
 
       all_data.store lang.to_s, json
     end 
+
+    puts "-- LocalioVASS: Transforming JSON to localio models --"
 
     languages_array.each do |lang| 
       all_data[lang.to_s].each do |key, value|
